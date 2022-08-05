a4d4a426abf390e01ccdcf241fda7b41d9d676fb
Ozodbekyum <hello7ozod@gamil.com>
2022-08-05 11:41:42 +0500

    logic18

--- bin/logical_operators18.dart
+++ bin/logical_operators18.dart
@@ -7,5 +7,15 @@ Given a five-digit integer a, check the following statement "All digits of the n
     Returns:
         bool: answer
 */
+bool func(int a) {
+  int t = a % 10;
+  int s = a ~/ 10 % 10;
+  int d = a ~/ 10 ~/ 10 % 10;
+  int f = a ~/ 10 ~/ 10 ~/ 10 % 10;
+  int g = a ~/ 10 ~/ 10 ~/ 10 ~/ 10 % 10;
+  return t < s && s < d && d < f && f < g;
+}
 
-void main() {}
+void main() {
+  print(func(98765));
+}
